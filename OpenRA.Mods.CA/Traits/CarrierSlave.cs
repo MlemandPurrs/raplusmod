fa987e3c85009d531d6a26d104a837a378f684f5
$MlemandPurrs <obvious.fake.is.obvious.11@gmail.com>
2021-04-15 03:52:43 +0300

    goes it this way

---
+++ OpenRA.Mods.CA/Traits/CarrierSlave.cs
@@ -0,0 +1,83 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Linq;
+using OpenRA.Mods.CA.Activities;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.CA.Traits
+{
+	[Desc("Can be slaved to a spawner.")]
+	public class CarrierSlaveInfo : BaseSpawnerSlaveInfo
+	{
+		[Desc("Move this close to the spawner, before entering it.")]
+		public readonly WDist LandingDistance = new WDist(5 * 1024);
+
+		public override object Create(ActorInitializer init) { return new CarrierSlave(init, this); }
+	}
+
+	public class CarrierSlave : BaseSpawnerSlave, INotifyIdle
+	{
+		readonly AmmoPool[] ammoPools;
+		public readonly CarrierSlaveInfo Info;
+
+		CarrierMaster spawnerMaster;
+
+		public CarrierSlave(ActorInitializer init, CarrierSlaveInfo info)
+			: base(init, info)
+		{
+			Info = info;
+			ammoPools = init.Self.TraitsImplementing<AmmoPool>().ToArray();
+		}
+
+		public void EnterSpawner(Actor self)
+		{
+			// Hopefully, self will be disposed shortly afterwards by SpawnerSlaveDisposal policy.
+			if (Master == null || Master.IsDead)
+				return;
+
+			// Proceed with enter, if already at it.
+			if (self.CurrentActivity is EnterCarrierMaster)
+				return;
+
+			// Cancel whatever else self was doing and return.
+			var target = Target.FromActor(Master);
+			self.QueueActivity(false, new EnterCarrierMaster(self, Master, spawnerMaster));
+		}
+
+		public override void LinkMaster(Actor self, Actor master, BaseSpawnerMaster spawnerMaster)
+		{
+			base.LinkMaster(self, master, spawnerMaster);
+			this.spawnerMaster = spawnerMaster as CarrierMaster;
+		}
+
+		bool NeedToReload(Actor self)
+		{
+			// The unit may not have ammo but will have unlimited ammunitions.
+			if (ammoPools.Length == 0)
+				return false;
+
+			return ammoPools.All(x => !x.HasAmmo);
+		}
+
+		void INotifyIdle.TickIdle(Actor self)
+		{
+			EnterSpawner(self);
+		}
+
+		public override void Stop(Actor self)
+		{
+			base.Stop(self);
+			EnterSpawner(self);
+		}
+	}
+}
---
+++ OpenRA.Mods.CA/Warheads/WarpDamageWarhead.cs
@@ -0,0 +1,33 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System.Collections.Generic;
+using OpenRA.GameRules;
+using OpenRA.Mods.CA.Traits;
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.Common.Warheads;
+
+namespace OpenRA.Mods.CA.Warheads
+{
+	[Desc("Affects warp value on the actors with Warpable trait.")]
+	public class WarpDamageWarhead : TargetDamageWarhead
+	{
+		protected override void InflictDamage(Actor victim, Actor firedBy, HitShape hitshape, WarheadArgs args)
+		{
+			var warpable = victim.TraitOrDefault<Warpable>();
+			if (warpable == null)
+				return;
+
+			var damage = Util.ApplyPercentageModifiers(Damage, args.DamageModifiers.Append(DamageVersus(victim, hitshape, args)));
+			warpable.AddDamage(damage, firedBy);
+		}
+	}
+}
